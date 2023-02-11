695ead251243dbfb1aa56b0ddefb5cb4cea49015
Artem Semirenko <artemiyka95@gmail.com>
2023-02-11 08:54:31 +0300

    improve stop command

--- src/main/java/dev/crifurch/unlimitedadmin/modules/stop/commands/CommandStop.java
+++ src/main/java/dev/crifurch/unlimitedadmin/modules/stop/commands/CommandStop.java
@@ -2,31 +2,61 @@ package dev.crifurch.unlimitedadmin.modules.stop.commands;
 
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.IntegerArgumentType;
-import com.mojang.brigadier.tree.CommandNode;
+import com.mojang.brigadier.arguments.StringArgumentType;
+import com.mojang.brigadier.context.CommandContext;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
-import net.minecraft.network.chat.TextComponent;
 
-import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class CommandStop {
     public static void register(CommandDispatcher<CommandSourceStack> dispatcher) {
-                dispatcher.register(
-                Commands.literal("stop").then(
-                        Commands.argument("seconds", IntegerArgumentType.integer(0, Integer.MAX_VALUE))
-                ).executes(context -> {
-                    if(!context.getSource().hasPermission(4)) {
-                        context.getSource().sendSuccess(new TextComponent("Эта команда доступна только для админа"), false);
-                        return 0;
-                    }
-            int seconds = 60;
-            if (context.getInput().split(" ").length > 1) {
-                seconds = Integer.parseInt(context.getInput().split(" ")[1]);
-            };
-            StopServerModule stopServerModule = new StopServerModule(context.getSource().getServer());
-            stopServerModule.stop(seconds);
-            return 1;
-        }));
+        dispatcher.register(
+                Commands.literal("serverstop").requires(
+                                (commandSourceStack) -> commandSourceStack.hasPermission(4)
+                        ).executes(CommandStop::stop)
+                        .then(Commands.argument("seconds", IntegerArgumentType.integer(0)).executes(CommandStop::stop))
+                        .then(Commands.argument("seconds", IntegerArgumentType.integer(0))
+                                .then(Commands.argument("reason", StringArgumentType.greedyString()).executes(CommandStop::stop))))
+        ;
     }
 
+    private static int stop(CommandContext<CommandSourceStack> context) {
+        int seconds = 60;
+        final String[] s = context.getInput().split(" ");
+        if (s.length > 1) {
+            seconds = Integer.parseInt(s[1]);
+        }
+        String reason = "Перезапуск сервера, скоро вернемся!";
+        if (s.length > 2) {
+            reason = Arrays.stream(s).skip(2).collect(Collectors.joining(" "));
+        }
+
+        StopServerModule stopServerModule = new StopServerModule(context.getSource().getServer());
+        stopServerModule.stop(seconds, reason);
+        return 0;
+    }
 }
+
+
+//dispatcher.register(
+//        Commands.literal("serverstop").requires((p_138665_) ->
+//        p_138665_.hasPermission(4)
+//        ).then(
+//        Commands.argument("seconds", IntegerArgumentType.integer(0, 10000)))
+//        .executes(context -> {
+//        int seconds = 60;
+//final String[] s = context.getInput().split(" ");
+//        if (s.length > 1) {
+//        seconds = Integer.parseInt(s[1]);
+//        }
+//        String reason = "Перезапуск сервера, скоро вернемся!";
+//        if (s.length > 2) {
+//        reason = Arrays.stream(s).skip(2).collect(Collectors.joining(" "));
+//        }
+//
+//        StopServerModule stopServerModule = new StopServerModule(context.getSource().getServer());
+//        stopServerModule.stop(seconds, reason);
+//        return 0;
+//        }));
--- src/main/java/dev/crifurch/unlimitedadmin/modules/stop/commands/StopServerModule.java
+++ src/main/java/dev/crifurch/unlimitedadmin/modules/stop/commands/StopServerModule.java
@@ -2,6 +2,9 @@ package dev.crifurch.unlimitedadmin.modules.stop.commands;
 
 import net.minecraft.network.chat.TextComponent;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.level.ServerPlayer;
+
+import java.util.List;
 
 public class StopServerModule {
     final MinecraftServer server;
@@ -11,17 +14,16 @@ public class StopServerModule {
         this.server = server;
     }
 
-    public void stop(int seconds) {
-         if (isStopping) {
+    public void stop(final int seconds, final String reason) {
+        if (isStopping) {
             return;
         }
-        final int delay = seconds;
 
         isStopping = true;
         new Thread(() -> {
-            int time = delay;
+            int time = seconds;
             while (time > 0) {
-                if (time == delay || time == 60 || time == 30 || time <= 15) {
+                if (time == seconds || time == 60 || time == 30 || time <= 15) {
                     sendMsg("\u00A7cСервер будет выключен через " + time + " секунд");
                 }
                 time--;
@@ -32,6 +34,9 @@ public class StopServerModule {
                 }
             }
             sendMsg("\u00A7cСервер выключается...");
+
+            final List<ServerPlayer> players = server.getPlayerList().getPlayers().subList(0, server.getPlayerList().getPlayers().size());
+            players.forEach(player -> player.connection.disconnect(new TextComponent("\u00A7cСервер выключен: " + reason)));
             server.halt(false);
         }).start();
     }

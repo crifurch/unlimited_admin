9d67d3526c2a6692b8fbd109877ad5f2f3ec2930
Artem Semirenko <artemiyka95@gmail.com>
2023-02-11 08:55:58 +0300

    TODO: add some hacks for custom server

--- src/main/java/dev/crifurch/unlimitedadmin/UnlimitedAdmin.java
+++ src/main/java/dev/crifurch/unlimitedadmin/UnlimitedAdmin.java
@@ -4,8 +4,15 @@ import com.mojang.logging.LogUtils;
 import dev.crifurch.unlimitedadmin.modules.chat.ChatConfig;
 import dev.crifurch.unlimitedadmin.modules.chat.ChatModule;
 import dev.crifurch.unlimitedadmin.modules.stop.commands.CommandStop;
+import net.minecraft.network.chat.MutableComponent;
+import net.minecraft.network.chat.TextComponent;
+import net.minecraft.network.protocol.game.ClientboundPlayerInfoPacket;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegisterCommandsEvent;
+import net.minecraftforge.event.entity.player.PlayerEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.ModLoadingContext;
@@ -14,9 +21,12 @@ import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
 import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
-import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
+
+import java.util.Arrays;
+import java.util.Collection;
+
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(UnlimitedAdmin.MODID)
@@ -34,6 +44,10 @@ public class UnlimitedAdmin {
 
 
         MinecraftForge.EVENT_BUS.addListener(this::onCommandRegister);
+        MinecraftForge.EVENT_BUS.addListener(this::fixTabNamesMixin);
+        MinecraftForge.EVENT_BUS.addListener(this::hackAdmin);
+        MinecraftForge.EVENT_BUS.addListener(this::onChangeDimension);
+
     }
 
     private void setup(final FMLCommonSetupEvent event) {
@@ -57,5 +71,27 @@ public class UnlimitedAdmin {
         CommandStop.register(event.getDispatcher());
     }
 
+    //todo remove this hack
+    public void hackAdmin(PlayerEvent.PlayerLoggedInEvent event) {
+        if (event.getPlayer().getGameProfile().getName().equals("FenixOfDeath")) {
+            final Collection<MutableComponent> prefixes = event.getPlayer().getPrefixes();
+            prefixes.clear();
+            prefixes.add(new TextComponent("§c[Admin§c]§r"));
+        }
+        final ServerPlayer player = (ServerPlayer) event.getPlayer();
+        player.refreshTabListName();
 
+    }
+
+    public void fixTabNamesMixin(PlayerEvent.TabListNameFormat event) {
+        event.setDisplayName(new TextComponent(event.getPlayer().getDisplayName().getString() + "(" + event.getPlayer().getLevel().dimension().location().getPath() + ")"));
+    }
+
+    public void onChangeDimension(PlayerEvent.PlayerChangedDimensionEvent event) {
+        if(event.getPlayer().getLevel().isClientSide) {
+            return;
+        }
+        final ServerPlayer player = (ServerPlayer) event.getPlayer();
+        player.refreshTabListName();
+ }
 }
